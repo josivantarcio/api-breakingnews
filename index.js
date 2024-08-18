b22d65c39ae5ff3c047e60a3ac464fc271f336d7
Josevan Oliveira <josivantarcio@msn.com>
2024-08-18 16:12:07 -0300

    refactor add router soma

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/soma", (req, res) => {
-    res.send ({soma: 5+5});
+    var soma = 56+11;
+    res.send ({soma: soma});
 });
 
 app.listen(3000 ); 
