138bf33964428a3d4aa262df657e06188580b20d
Josevan Oliveira <josivantarcio@msn.com>
2024-08-18 13:40:53 -0300

    studying function

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.get("/ ", (req, res) => {
-    res.send("ola mundo");
+app.get("/soma", (req, res) => {
+    res.send ({soma: 5+5});
 });
 
-app.listen(3000); 
+app.listen(3000 ); 
